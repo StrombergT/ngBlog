415b7c722e1e49973b51d90e4e18776a9efc71ed
StrombergT <tpr.stromberg@gmail.com>
2023-10-31 11:04:07 +0100

    created post-detail

---
+++ ng-blog/src/app/components/post-detail/post-detail.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from 'src/app/services/post.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { Post } from 'src/app/models/post';
+import { Comment } from 'src/app/models/comment';
+import { ViewService } from 'src/app/services/view.service';
+
+@Component({
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.css'],
+})
+export class PostDetailComponent implements OnInit {
+  post: Post | undefined;
+  comments: Comment[] = [];
+  newComment: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private commentService: CommentService,
+    public viewService: ViewService
+  ) {}
+
+  ngOnInit(): void {
+    const postId = +this.route.snapshot.paramMap.get('id')!;
+    this.post = this.postService.getPostById(postId);
+    if (this.post) {
+      this.comments = this.commentService.getCommentsForPost(postId);
+    }
+  }
+
+  public likePost(postId: number) {
+    if (this.post) {
+      this.postService.likePost(postId);
+      this.post = this.postService.getPostById(postId);
+    }
+  }
+
+  public dislikePost(postId: number) {
+    if (this.post) {
+      this.postService.dislikePost(postId);
+      this.post = this.postService.getPostById(postId);
+    }
+  }
+
+  public addComment(postId: number, newComment: string) {
+    if (newComment.trim() !== '') {
+      const userName = this.viewService.isAdmin() ? 'Admin' : 'User';
+      this.commentService.addComment(postId, newComment, userName);
+      this.comments = this.commentService.getCommentsForPost(postId);
+      this.newComment = '';
+    }
+  }
+
+  public removePost(postId: number) {
+    if (this.viewService.isAdmin()) {
+      this.postService.removePost(postId);
+    }
+  }
+}
