4d999991216b0c8afe3dd141c27dbd1be1683698
StrombergT <tpr.stromberg@gmail.com>
2023-10-31 10:46:24 +0100

    changes in home

--- ng-blog/src/app/views/home/home.component.ts
+++ ng-blog/src/app/views/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostService } from 'src/app/services/post.service';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: Post[] = [];
 
+  constructor(private postService: PostService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+  }
+
+  navigateToPostDetails(postId: number): void {
+    this.router.navigate(['/post', postId]);
+  }
 }
